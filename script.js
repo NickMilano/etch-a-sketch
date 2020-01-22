f2af3c3bc792fa715e8559b8962eeb8177818ade
NickMilano <nickalos.milano@gmail.com>
2020-01-21 19:22:28 -0500

    added color functionality

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 const board = document.querySelector('.container');
+const pen = document.getElementById('pen');
+let color = pen.value;
 buildBoard(16);
+
+function boxColor() {
+  console.log(color)
+    if(color == "rainbow")
+      this.style.backgroundColor = "#"+((1<<24)*Math.random()|0).toString(16);
+    else
+      this.style.backgroundColor = `${color}`;
+}
 
 function buildBoard(size) {
 
@@ -8,24 +18,34 @@ function buildBoard(size) {
 
   while(board.firstChild)
     board.removeChild(board.firstChild);
-    
+
   for(let i = 0; i < size*size; i++) {
-    const box = document.createElement('div');
+    let box = document.createElement('div');
     box.classList.add('new');
+    box.addEventListener("mouseover", boxColor);
     board.appendChild(box);
   }
 
-  const boxes = document.querySelectorAll('.new');
-  boxes.forEach((box) => {
-    box.addEventListener('mouseover', (e) => {
-      box.setAttribute('style', 'background: black');
-    });
-  });
 
 }
 
 const reset = document.querySelector('#reset');
   reset.addEventListener('click', (e) => {
-    var num = window.prompt("Enter Size of New Board:", "1-64");
-    buildBoard(num);
+      const boxes = document.querySelectorAll('.new');
+      boxes.forEach((box) => {
+        box.setAttribute('style', 'backgroud: lightgrey');
+      });
+  });
+
+const newBoard = document.querySelector('#resize');
+  newBoard.addEventListener('click', (e) => {
+    let num = 0;
+    while(num < 1 || num > 64 || isNaN(num)) {
+      num = window.prompt("Enter Size of New Board: (1-64)");
+      buildBoard(num);
+    }
 });
+
+  pen.addEventListener('change', (e) => {
+    color = pen.value;
+  });
